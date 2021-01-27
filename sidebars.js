8a2a6ee6b647fe22a549846623449930034fbfc0
Ben Zuill-Smith <bzuillsmith@gmail.com>
2021-01-26 20:24:16 -0800

    removed duplicate document and formatted sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,12 +1,31 @@
 module.exports = {
   someSidebar: {
-    "Elevation Tabs": ['elevationtabs/layouttab','elevationtabs/splicetab',
-    `elevationtabs/doorstab`,`elevationtabs/unequaltab`, `elevationtabs/wmergetab`,
-    `elevationtabs/steptab`,`elevationtabs/raketab`,`elevationtabs/archtab`,
-    `elevationtabs/bulktab`, `elevationtabs/heighttab`, `elevationtabs/horztab`,
-    `elevationtabs/eqhoztab`, `elevationtabs/splittab`, `elevationtabs/mitertab`,
-    `elevationtabs/stoptab`, `elevationtabs/altverttab`, `elevationtabs/excludetab`,
-    `elevationtabs/glasstab`],
-  Docusaurus: ['sharingcatalogs','glossary', `optimizer`,`importexportjobs`,`cansystems`],
+    'Elevation Editor Tabs': [
+      'elevationtabs/layouttab',
+      'elevationtabs/splicetab',
+      'elevationtabs/doorstab',
+      'elevationtabs/unequaltab',
+      'elevationtabs/wmergetab',
+      'elevationtabs/steptab',
+      'elevationtabs/raketab',
+      'elevationtabs/archtab',
+      'elevationtabs/bulktab',
+      'elevationtabs/heighttab',
+      'elevationtabs/horztab',
+      'elevationtabs/eqhoztab',
+      'elevationtabs/splittab',
+      'elevationtabs/mitertab',
+      'elevationtabs/stoptab',
+      'elevationtabs/altverttab',
+      'elevationtabs/excludetab',
+      'elevationtabs/glasstab'
+    ],
+    'Misc': [
+      'sharingcatalogs',
+      'glossary',
+      'optimizer',
+      'importexportjobs',
+      'cansystems'
+    ],
   },
 };
